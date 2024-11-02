92157f6af3dcab2f015c5f28071f39e9fd5f83e1
mortezakdevelop <m.vortex3537007@gmail.com>
2024-11-01 18:19:16 -0800

    Design register screen

--- app/src/main/java/com/example/recipeexplore/MainActivity.kt
+++ app/src/main/java/com/example/recipeexplore/MainActivity.kt
@@ -5,13 +5,24 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import com.example.recipeexplore.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     // apiKey 03573bc48ac34b99bc9c09c84a1931d1
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navHost:NavHostFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navHost = supportFragmentManager.findFragmentById(R.id.navHost) as NavHostFragment
+    }
+
+    override fun onNavigateUp(): Boolean {
+        return navHost.navController.navigateUp() || super.onNavigateUp()
     }
 }
